d053df7964cb77b4b81c94c484858cf571d41cc7
Yehoshuaro <231132@astanait.edu.kz>
2024-01-25 23:23:22 +0600

    I will end it until Sunday

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+// Код не дописан, допишу его до воскресенья
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
     }
+
 }
--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class Person {
+public class Person {
+
+    private int id;
+    private String name;
+    private String surname;
+
+    public Person() {
+        id = 1;
+    }
+
+    public Person(String name, String surname) {
+        id = 1;
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("ID %d: %s %s", id, name, surname);
+    }
 }
